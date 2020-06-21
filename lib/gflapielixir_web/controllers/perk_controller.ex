1c7b8f930b243eb5c5d026769db873718e779c26
Christian Deacon <gamemann@gflclan.com>
2020-06-21 14:32:35 +0000

    Implemented Ecto query.

--- lib/gflapielixir_web/controllers/perk_controller.ex
+++ lib/gflapielixir_web/controllers/perk_controller.ex
@@ -1,9 +1,21 @@
 defmodule GflapielixirWeb.PerkController do
   use GflapielixirWeb, :controller
+  alias Gflapielixir.Repo
 
-  #import Ecto.Query
+  import Ecto.Query
 
+  @spec showgroup(Plug.Conn.t(), map) :: Plug.Conn.t()
   def showgroup(conn, %{"steamid" => steamid} = _params) do
-    json(conn, %{test: "haha"})
+    query = from u in "users", where: u.steamid == ^steamid, select: u.group
+
+    group = Repo.one(query)
+
+    {group, error} = if group == nil do
+      {0, 404}
+    else
+      {group, -1}
+    end
+
+    json(conn, %{steamid: steamid, group: group, error: error})
   end
 end
